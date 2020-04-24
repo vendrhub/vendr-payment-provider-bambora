93634f268a4b7e850d15d8edff5ebb4d72701a57
Bjarne Fyrstenborg <bf@oerskov-gruppen.lan>
2020-04-24 12:22:06 +0200

    Add base payment provider

---
+++ src/Vendr.PaymentProviders.Bambora/BamboraCheckoutOneTimeSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vendr.PaymentProviders.Bambora
+{
+    public class BamboraCheckoutOneTimeSettings : BamboraSettingsBase
+    {
+
+    }
+}
